2f68f415d542e6d7db29b99aa050b625d001fa37
Manjari Dissanayake <manji@Manjaris-MacBook-Pro.local>
2019-03-28 09:22:07 -0400

    Capstone github conneting assignment. Capstone project will be on this notebook

---
+++ github_connect.py
@@ -0,0 +1,9 @@
+#Hi! 
+
+#Welcome to Manji's **IBM Applied Data Science Capstone page.**
+
+#This will be the page I will use for my capstone project.
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
